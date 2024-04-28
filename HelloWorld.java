7d619a83273ef8a8f40d17e0e8552fd34f69ceea
husainyasir <167346903+husainyasir@users.noreply.github.com>
2024-04-29 00:37:41 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,7 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
         System.out.println("Hello, yasir ! you are doing testing of JOB 3-4 ");
-        System.out.println("Good Luck!");
+        System.out.println("Good Night!");
+        
     }
 }
